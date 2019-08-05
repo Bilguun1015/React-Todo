7ebd84baa7afe796d85018864f48d3ebffa7fe3d
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-08-05 15:03:38 -0500

    finished toggling through adding className=completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+
+import './components/TodoComponents/Todo.css';
+
+const todoData = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor () {
+    super();
+    this.state = {
+      todoData
+    };
+  }
+
+  toggleItem = id => {
+    console.log(id)
+    this.setState({
+      todoData: this.state.todoData.map(item => {
+        if(item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <div>
+
+          <TodoList todoData={this.state.todoData} 
+          toggleItem={this.toggleItem}
+
+          />
+        </div>
       </div>
     );
   }
